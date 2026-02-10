5435029aff4119367c529d55e94d773762221984
syf <1071343549@qq.com>
2026-02-10 14:58:32 +0800

    chore: stop tracking generated caches and node_modules

--- enterprise_pw/.cache-synpress/3fe1923a802259080f68_en-US_langfix2/capsolver-extension/assets/aW5qZWN0X2hhc2g/aW5qZWN0X2hhc2g-s.js
+++
@@ -1,7 +0,0 @@
-(function () {
-  window.addEventListener('message', function (event) {
-    if (!['capsolverCallback', 'capsolverDetectedCallback', 'capsolverFailedCallback'].includes(event.data.type))
-      return;
-    window[event.data.callback] && window[event.data.callback](event.data?.data);
-  });
-})();
